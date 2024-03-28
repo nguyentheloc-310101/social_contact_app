bb92fa7a07efcd7e9c2bfaa25b5cbea1732b2f87
loc-dev <nguyentheloc310101@gmail.com>
2024-03-28 13:46:55 +0700

    feat(mySQL): new storage for db

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,6 +1,6 @@
 const AuthLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="h-full flex items-center justify-center">{children}</div>
+    <div className="h-[100vh] flex items-center justify-center">{children}</div>
   );
 };
 
